162e50b01d0a67f5ecb0f3b1843a0df2c84ca427
Ivan Collado <ivancoto03@gmail.com>
2023-12-04 13:42:00 +0100

    Added complete code for Day4

---
+++ Day4/README.md
@@ -0,0 +1,5 @@
+
+## Day 4: Scratchcards
+
+Source: [Advent of Code 2023 - Day 4](https://adventofcode.com/2023/day/4)
+
---
+++ Day4/code.py
@@ -0,0 +1,38 @@
+with open('input.txt', 'r') as f:
+    data = f.read()
+
+data = data.strip().splitlines()
+
+resultPart1 = 0
+counterIds = {}
+
+for text in data:
+    splittedText = text.split(':')
+    cardId = int(splittedText[0].split()[1])
+    numbers = splittedText[1].split('|')
+
+    winningNumbers = numbers[0].strip().split()
+    myNumbers = numbers[1].strip().split()
+
+    counterPart1 = 0
+    counterPart2 = 0
+    for number in myNumbers:
+        if number in winningNumbers:
+            counterPart1 = 1 if counterPart1 == 0 else counterPart1 * 2
+            counterPart2 += 1
+
+    for x in range(0, counterPart2 + 1):
+        nextId = cardId + x
+
+        if nextId in counterIds:
+            counterIds[nextId] += counterIds[cardId] if cardId != nextId else 1
+        else:
+            counterIds[nextId] = counterIds[cardId] if cardId != nextId else 1
+
+
+
+    resultPart1 += counterPart1
+
+print(f"Part 1: {resultPart1}")
+print(f"Part 2: {sum([counterIds[i] for i in counterIds])}")
+
