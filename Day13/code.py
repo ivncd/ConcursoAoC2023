74715267a07a0923fbea4010966198ac6ef215be
Ivan Collado <ivancoto03@gmail.com>
2023-12-13 20:16:07 +0100

    Refactor

--- Day13/code.py
+++ Day13/code.py
@@ -1,11 +1,9 @@
 with open('input.txt', 'r') as f:
     data = f.read()
 
-
 patterns = data.strip().split('\n\n')
 
-
-def checkHorizontal(pattern, possibleStart):
+def checkHorizontal(pattern : list, possibleStart : int) -> bool:
     minId = min(len(pattern) - (possibleStart + 2), possibleStart)
     isHorizontal = True
     for y in range(1, minId + 1):
@@ -15,8 +13,7 @@ def checkHorizontal(pattern, possibleStart):
     
     return isHorizontal
 
-
-def checkVertical(pattern, possibleStart):
+def checkVertical(pattern : list, possibleStart: int) -> bool:
     minId = min(len(pattern[0]) - (possibleStart + 2), possibleStart)
     isVertical = True
     for y in range(len(pattern)):
@@ -26,15 +23,13 @@ def checkVertical(pattern, possibleStart):
 
     return isVertical
 
-
-resultPattern = {}
-resultPart1 = 0
-for pId, pattern in enumerate(patterns):
-    pattern = pattern.splitlines()
+def getPatternResult(pattern : list, notAllowedResult : int = 0) -> int:
+    result = 0
     for rowId in range(len(pattern) - 1):
-        if pattern[rowId] == pattern[rowId + 1] and checkHorizontal(pattern, rowId):
-            result = (rowId + 1) * 100
-            break
+        if pattern[rowId] == pattern[rowId + 1]:
+            if checkHorizontal(pattern, rowId) and (rowId + 1) * 100 != notAllowedResult:
+                result = (rowId + 1) * 100
+                break
         
     else: 
         for columnId in range(len(pattern[0]) - 1):
@@ -43,48 +38,36 @@ for pId, pattern in enumerate(patterns):
                     break
             
             else:
-                if checkVertical(pattern, columnId):
+                if checkVertical(pattern, columnId) and (columnId + 1) != notAllowedResult:
                     result = columnId + 1
                     break
+
+
+    return result
+
+
+resultPart1, resultPattern = 0, {}
+for pId, pattern in enumerate(patterns):
+    pattern = pattern.splitlines()
+    result = getPatternResult(pattern)
     
     resultPart1 += result
     resultPattern[pId] = result
 
 
-"""
-For later:
-
-Refactor the code and create a function with the solving to not create another for in part2
-"""
-
 resultPart2 = 0
 for pId, pattern in enumerate(patterns):
     lastResult = resultPart2
-    for letterId in range(len(pattern)): # BRUTEFORCE PART 2
+    for letterId in range(len(pattern)):
         newPattern = (pattern[:letterId] + ('.' if pattern[letterId] == '#' else ('#' if pattern[letterId] == '.' else pattern[letterId])) + pattern[letterId + 1:]).splitlines()
-        for rowId in range(len(newPattern) - 1):
-            if newPattern[rowId] == newPattern[rowId + 1]:
-                if checkHorizontal(newPattern, rowId) and (rowId + 1) * 100 != resultPattern[pId]:
-                    resultPart2 += (rowId + 1) * 100
-                    break
-            
-        else:
-            for columnId in range(len(newPattern[0]) - 1):
-                for y in range(len(newPattern)):
-                    if newPattern[y][columnId] != newPattern[y][columnId + 1]:
-                        break
-                
-                else:
-                    if checkVertical(newPattern, columnId) and ((columnId + 1)) != resultPattern[pId]:
-                        resultPart2 += columnId + 1
-                        break
-        
-        if resultPart2 > lastResult:
-            break 
-    
-    else:
-        resultPart2 += resultPattern[pId]
+        result = getPatternResult(newPattern, resultPattern[pId])
+        if result != 0: # Invalid pattern, no mirror
+            break
 
+    else: # In case there is no break in the loop then the smudge dont affect our solution so we just take the data
+        result = resultPattern[pId]
+
+    resultPart2 += result
 
 
 print('Part 1:', resultPart1)
