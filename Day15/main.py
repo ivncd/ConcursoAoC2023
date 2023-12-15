1838d4c917ff2c42eef861f2e59f39df45311a2e
Ivan Collado <ivancoto03@gmail.com>
2023-12-15 18:48:30 +0100

    Update main.py

--- Day15/main.py
+++ Day15/main.py
@@ -24,21 +24,17 @@ for string in data:
             if label in lensBox[hashNumber]:
                 lensBox[hashNumber].remove(label)
 
-    elif '=' in string:
+    else:
         label, value = string.split('=')
         hashNumber = getHASHNumber(label) 
         if hashNumber in lensBox:
             if label not in lensBox[hashNumber]:
                 lensBox[hashNumber].append(label)
-                lensValue[label] = int(value)
-            
-            else:
-                lensValue[label] = int(value)
         
         else:
             lensBox[hashNumber] = [label]
-            lensValue[label] = int(value)
-
+            
+        lensValue[label] = int(value)
 
     # Part 1
     resultPart1 += getHASHNumber(string)
@@ -47,8 +43,7 @@ for string in data:
 resultPart2 = 0
 for box in lensBox:
     for id, label in enumerate(lensBox[box]):
-        result = (1 + box) * (id + 1) * lensValue[label]
-        resultPart2 += result
+        resultPart2 += (1 + box) * (id + 1) * lensValue[label]
 
 
 
